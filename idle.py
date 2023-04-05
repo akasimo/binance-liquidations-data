70a725966bb824c5290dce2f1dc98bdc8279aad6
akasimo <email@example.com>
2023-04-05 16:41:06 +0300

    remove garbage

--- idle.py
+++
@@ -1,9 +0,0 @@
-import time
-
-while True:
-    print("Hello World")
-    # create file named "hello.txt" and write "Hello World" in it
-    with open("hello.txt", "w") as f:
-        f.write("Hello World")
-    # wait for 1 second
-    time.sleep(1)
